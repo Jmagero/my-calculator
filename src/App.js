6ec80620daf05098a00bd63c056e78539e4702f0
Jocyline Magero <jmagero2017@gmail.com>
2021-08-17 17:05:10 -0500

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
+class App extends React.PureComponent {
   render() {
     return (<Calculator />);
   }
