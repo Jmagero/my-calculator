654d8b3ad2f544bf4d98c9e7539c89c5cc33037a
Jocyline Magero <jmagero2017@gmail.com>
2021-08-18 17:07:31 -0500

    Add event function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,7 +7,25 @@ class Calculator extends React.Component {
   constructor(props){
     super(props)
     this.state = {}
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(event){
+    const text = event.target.value;
+    const objstate = this.state;
+    const newState = calculate(objstate, text);
+    this.setState(newState);
+    const calculatorDisplay = document.querySelector('.calculator-display');
+    if (newState.total === null && newState.next === null && newState.operation === null) {
+      calculatorDisplay.innerHTML = '0';
+    } else if (newState.next !== null) {
+      calculatorDisplay.textContent = newState.next;
+    } else if (newState.next === null && newState.total !== null) {
+      calculatorDisplay.textContent = newState.total;
+    }
+  }
+
+
   render() {
     return (
       <div className="wrapper">
