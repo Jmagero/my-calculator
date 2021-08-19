67458e262912628abe81dc4dc115cc522f310289
Jocyline Magero <jmagero2017@gmail.com>
2021-08-19 12:38:06 -0500

    refractor with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,18 +1,14 @@
-import React from 'react';
+/* eslint-disable */ 
+import React, { useState }  from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
+const  Calculator = () => {
+  const [state, setState] = useState({ total: '0'});
+  const handleClick = (event) =>  {
     const text = event.target.value;
-    const objstate = this.state;
+    const objstate = state;
     const newState = calculate(objstate, text);
-    this.setState(newState);
+    setState(newState);
     const calculatorDisplay = document.querySelector('.calculator-display');
     if (newState.total === null && newState.next === null && newState.operation === null) {
       calculatorDisplay.innerHTML = '0';
@@ -22,43 +18,40 @@ class Calculator extends React.Component {
       calculatorDisplay.textContent = newState.total;
     }
   }
-
-  render() {
     return (
       <div className="wrapper">
         <div className="button-row calculator-display">0</div>
         <div className="button-row">
-          <button type="button" value="AC" onClick={this.handleClick}>AC</button>
-          <button type="button" value="+/-" onClick={this.handleClick}>+/-</button>
-          <button type="button" value="%" onClick={this.handleClick}>%</button>
-          <button className="button-right" type="button" value="÷" onClick={this.handleClick}>÷</button>
+          <button type="button" value="AC" onClick={handleClick}>AC</button>
+          <button type="button" value="+/-" onClick={handleClick}>+/-</button>
+          <button type="button" value="%" onClick={handleClick}>%</button>
+          <button className="button-right" type="button" value="÷" onClick={handleClick}>÷</button>
         </div>
         <div className="button-row">
-          <button type="button" value="7" onClick={this.handleClick}>7</button>
-          <button type="button" value="8" onClick={this.handleClick}>8</button>
-          <button type="button" value="9" onClick={this.handleClick}>9</button>
-          <button className="button-right" type="button" value="x" onClick={this.handleClick}>*</button>
+          <button type="button" value="7" onClick={handleClick}>7</button>
+          <button type="button" value="8" onClick={handleClick}>8</button>
+          <button type="button" value="9" onClick={handleClick}>9</button>
+          <button className="button-right" type="button" value="x" onClick={handleClick}>*</button>
         </div>
         <div className="button-row">
-          <button type="button" value="4" onClick={this.handleClick}>4</button>
-          <button type="button" value="5" onClick={this.handleClick}>5</button>
-          <button type="button" value="6" onClick={this.handleClick}>6</button>
-          <button type="button" value="-" onClick={this.handleClick} className="button-right">-</button>
+          <button type="button" value="4" onClick={handleClick}>4</button>
+          <button type="button" value="5" onClick={handleClick}>5</button>
+          <button type="button" value="6" onClick={handleClick}>6</button>
+          <button type="button" value="-" onClick={handleClick} className="button-right">-</button>
         </div>
         <div className="button-row">
-          <button type="button" value="1" onClick={this.handleClick}>1</button>
-          <button type="button" value="2" onClick={this.handleClick}>2</button>
-          <button type="button" value="3" onClick={this.handleClick}>3</button>
-          <button className="button-right" type="button" value="+" onClick={this.handleClick}>+</button>
+          <button type="button" value="1" onClick={handleClick}>1</button>
+          <button type="button" value="2" onClick={handleClick}>2</button>
+          <button type="button" value="3" onClick={handleClick}>3</button>
+          <button className="button-right" type="button" value="+" onClick={handleClick}>+</button>
         </div>
         <div className="button-row">
-          <button className="zero" type="button" value="0" onClick={this.handleClick}>0</button>
-          <button type="button" value="-" onClick={this.handleClick}>-</button>
-          <button className="button-right" type="button" value="=" onClick={this.handleClick}>=</button>
+          <button className="zero" type="button" value="0" onClick={handleClick}>0</button>
+          <button type="button" value="-" onClick={handleClick}>-</button>
+          <button className="button-right" type="button" value="=" onClick={handleClick}>=</button>
         </div>
       </div>
     );
-  }
 }
 
 export default Calculator;
