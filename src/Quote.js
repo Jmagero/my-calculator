012f0d7c885978a27f0926a83eec9b84e52cbc01
Jocyline Magero <jmagero2017@gmail.com>
2021-08-24 14:45:49 -0500

    Add Quote page

---
+++ src/Quote.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import React from 'react';
+const Quote = () => {
+    return(
+        <React.Fragment>
+            <p style={{
+                fontSize: "2rem",
+                textAlign: "center",
+                margin: "0 0.5rem"
+            }}>
+                Mathematics is not about numbers,equations,
+                computations or algorithms: its about understanding-
+                Willian Paul Thurston
+            </p>
+        </React.Fragment>
+    )
+}
+
+export default Quote;
