36cd15f39af73c0f8bae1d8c51113413398bccd7
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-08-26 00:09:13 +0100

    Add calculate function test

---
+++ src/test/calculate.test.js
@@ -0,0 +1,28 @@
+import calculate from '../logic/calculate';
+
+describe('Tests for calculate function when ', () =>{
+    test('Should return a null obj button is AC',() =>{
+        const obj = {};
+        expect(calculate(obj,"AC")).toEqual({total:null, next: null,
+            operation: null,})
+    })
+
+    test('Should return a 0 obj when button is 0 && obj.next is 0',() =>{
+        expect(calculate({total:null,next:"0",operation:null},"0")).toStrictEqual
+        ({total:null, next: "0",
+            operation: null})
+    })
+
+    test('It concatenates the a number with the next value',() =>{
+        expect(calculate({total:null,next:"4",operation:"+"},"2")).toStrictEqual
+        ({total:null, next: "42",
+            operation: "+"})
+    })
+
+    test('Update the next when there is no operation',() =>{
+        expect(calculate({total:null,next:null,operation:"+"},"2")).toStrictEqual
+        ({total:null, next: "2",
+            operation: "+"})
+    })
+
+})
