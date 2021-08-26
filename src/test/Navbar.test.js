06f118c3a02977710e1874e4256b1c769985ea19
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-08-26 19:23:08 +0100

    Add Calculator and NavBar test

---
+++ src/test/Navbar.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {screen, render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Calculator from '..components/NavBar';
+
+it('renders NavBar', () => {
+    const tree = renderer.create(NavBar)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders Home link in NavBar component', () => {
+    render(
+    <BrowserRouter>
+       <NavBar />
+    </BrowserRouter>
+    );
+    expect(screen.getByText('Home')).toBeInTheDocument();
+});
+
+it('renders Quote link in NavBar component', () => {
+    render(
+        <BrowserRouter>
+         <NavBar />
+        </BrowserRouter>
+     );
+    expect(screen.getByText('Quote')).toBeInTheDocument();
+});
